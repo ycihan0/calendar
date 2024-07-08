a2728c9db6923b29353ab393a8fc5aa79a1d6e5b
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-07-08 15:42:38 +0300

    install toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 import Home from "./components/Home/Home";
 
+
+
 function App() {
   return (
     <Routes>
+       
       <Route path="/" element={<Auth />} />
       <Route path="/home" element={<Home />} />
     </Routes>
--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -2,27 +2,26 @@ import { useFormik } from "formik";
 import { userSchema } from "../../schema/userSchema";
 import PropTypes from "prop-types";
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 const Login = ({ toggleForm }) => {
-
   const onSubmit = async (values, actions) => {
     try {
       const res = await axios.post(
         `${import.meta.env.VITE_API_BASE_URL}/users/login`,
         values
       );
-       if (res.status === 200) {
+      if (res.status === 200) {
         actions.resetForm();
         localStorage.setItem("user", JSON.stringify(res.data));
-        // toast.success("Admin Login Success!");
-        // push("/admin/profile");
+        toast.success('Giriş Başarılı')
+        window.location.href = "/home";
       }
     } catch (error) {
       console.log(error);
+      toast.error('Tekrar deneyiniz');
     }
   };
-
- 
 
   const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
     useFormik({
@@ -34,27 +33,29 @@ const Login = ({ toggleForm }) => {
     });
 
   return (
-    <div  className="center" >
-      <form onSubmit={handleSubmit} >
-      <h2 className="form-title" id="login" onClick={toggleForm}>
-        <span>veya</span>Giriş yap
-      </h2>
-      <div className="form-holder" >
-        <input
-          type="email"
-          className="input"
-          placeholder="Email"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-      </div>
-      {touched.email && <span className="error-message">{errors.email}</span>}
+    <div className="center">
+     
+      <form onSubmit={handleSubmit}>
+        <h2 className="form-title" id="login" onClick={toggleForm}>
+          <span>veya</span>Giriş yap
+        </h2>
+        <div className="form-holder">
+          <input
+            type="email"
+            className="input"
+            placeholder="Email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
+        {touched.email && <span className="error-message">{errors.email}</span>}
 
-      <button type="submit" className="submit-btn" >
-        Giriş yap
-      </button>
+        <button type="submit" className="submit-btn">
+          Giriş yap
+        </button>
+        
       </form>
     </div>
   );
