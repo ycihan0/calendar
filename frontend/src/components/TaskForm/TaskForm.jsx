234e8a129eef900dc9d6c17aedb0cdffb5331123
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-07-08 19:36:29 +0300

    task form validatoions

--- frontend/src/components/TaskForm/TaskForm.jsx
+++ frontend/src/components/TaskForm/TaskForm.jsx
@@ -1,47 +1,138 @@
+import { useFormik } from "formik";
 import "./TaskForm.scss";
+import { taskSchema } from "../../schema/taskSchema";
+import { useState } from "react";
 
 const TaskForm = () => {
-  return (
-<div className="background_bg">
-<div className="contact_section layout_padding">
-    <div className="container">
-       <h1 className="watchs_taital">01<br/>OLAY EKLE</h1>
-    </div>
-    <div className="contact_section_2">
-       <div className="container-fluid">
-          <div className="row">
-             <div className="col-md-6">
-                <div className="mail_section_1">
-                <div className="input-group">
-                    <input type="text" className="mail_text" placeholder="Başlık" name="title"/>
-                </div>
-                 <div className="input-group">
-                    <label className="input-label" htmlFor="startTime">Başlangıç Tarihi: </label>
-                    <input type="datetime-local" className="mail_text" placeholder="Başlangıç Tarihi" name="Phone Number"/>
-                 </div>
-                 <div className="input-group">
-                    <label className="input-label" htmlFor="startTime">Bitiş Tarihi: </label>
-                    <input type="datetime-local" className="mail_text" placeholder="Bitiş Tarihi" name="Email"/>
-                 </div>
-                   <div className="input-group">
-                     <select className="mail_text"  name="options" defaultValue="">
-                        <option value="" disabled>Lütfen bir seçenek belirleyin</option>
-                        <option value="option1">Bu bir görev🗒️</option>
-                        <option value="option2">Bu bir etkinlik🎉 </option>
-                     </select>
-                   </div>
-                   <div className="send_bt"><a href="#">EKLE</a></div>
-                </div>
-             </div>
-             <div className="col-md-6 padding_right_0">
-                <div className="map_section"><img src="src/assets/images/map-img.png"/></div>
-             </div>
-          </div>
-       </div>
-    </div>
- </div>
- </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState("");
 
-export default TaskForm
+  const handleChangeOption = (e) => {
+    setSelectedOption(e.target.value);
+    console.log(selectedOption);
+  };
+
+  const onSubmit = async (values, actions) => {
+   console.log("here")
+    // try {
+    //   const res = await axios.post(
+    //     `${import.meta.env.VITE_API_BASE_URL}/users`,
+    //     values
+    //   );
+    //   if (res.status === 201) {
+    //     actions.resetForm();
+    //     toast.success("Kayıt Başarılı");
+    //     toggleForm();
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    //   toast.error("Kullanıcı zaten kayıtlı");
+    // }
+  };
+
+  const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
+    useFormik({
+      initialValues: {
+        title: "",
+        startDate: "",
+        endDate: "",
+      },
+      onSubmit,
+      validationSchema: taskSchema,
+    });
+
+  return (
+    <div className="background_bg">
+      <div className="contact_section layout_padding">
+        <div className="container">
+          <h1 className="watchs_taital">
+            01
+            <br />
+            OLAY EKLE
+          </h1>
+        </div>
+        <div className="contact_section_2">
+          <div className="container-fluid">
+            <form className="row" onSubmit={handleSubmit}>
+              <div className="col-md-6">
+                <div className="mail_section_1">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="mail_text"
+                      placeholder="Başlık"
+                      name="title"
+                      value={values.title}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </div>
+                  {touched.title && (
+                    <span className="error-messages">{errors.title}</span>
+                  )}
+                  <div className="input-group">
+                    <label className="input-label" htmlFor="startTime">
+                      Başlangıç Tarihi:
+                    </label>
+                    <input
+                      type="datetime-local"
+                      className="mail_text"
+                      placeholder="Başlangıç Tarihi"
+                      name="startDate"
+                      value={values.startDate}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {touched.startDate && (
+                      <span className="error-messages">{errors.startDate}</span>
+                    )}
+                  </div>
+                  <div className="input-group">
+                    <label className="input-label" htmlFor="startTime">
+                      Bitiş Tarihi:
+                    </label>
+                    <input
+                      type="datetime-local"
+                      className="mail_text"
+                      placeholder="Bitiş Tarihi"
+                      name="endDate"
+                      value={values.endDate}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {touched.endDate && (
+                      <span className="error-messages">{errors.endDate}</span>
+                    )}
+                  </div>
+                  <div className="input-group">
+                    <select
+                      className="mail_text"
+                      name="options"
+                      value={selectedOption}
+                      onChange={handleChangeOption}
+                    >
+                      <option value="" disabled>
+                        Lütfen bir seçenek belirleyin
+                      </option>
+                      <option value="task">Bu bir görev🗒️</option>
+                      <option value="event">Bu bir etkinlik🎉 </option>
+                    </select>
+                  </div>
+                  <div type="submit" className="send_bt">
+                    <a href="#">EKLE</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 padding_right_0">
+                <div className="map_section">
+                  <img src="src/assets/images/map-img.png" />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskForm;
---
+++ frontend/src/schema/taskSchema.js
@@ -0,0 +1,13 @@
+import * as Yup from "yup";
+export const taskSchema = Yup.object({
+  title: Yup.string()
+    .required("Bir başlık giriniz.")
+    .min(3, "En az 3 karakter giriniz."),
+  startDate: Yup.date().required("Lütfen bir başlangıç tarih giriniz."),
+  endDate: Yup.date()
+    .required("Lütfen bir bitiş tarihi giriniz.")
+    .min(
+      Yup.ref("startDate"),
+      "Bitiş tarihi, başlangıç tarihinden sonra olmalıdır."
+    ),
+});
